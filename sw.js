dc64f7197b8aad73e220eb16d757ca5b12880db5
ModestlyMoist <brant@ziembas.net>
2026-09-15 16:44:14 -0700

    Refresh cache for recurring editor deletion fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v43';
+const C = 'family-hub-v44';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './styles.css?v=38',
-        './app.js?v=42',
+        './app.js?v=43',
         './manifest.webmanifest'
       ])
     )
