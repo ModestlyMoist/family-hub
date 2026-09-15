486f876cc9128752cbf9d0f50943d0b44a26022e
ModestlyMoist <brant@ziembas.net>
2026-09-15 16:23:29 -0700

    Refresh cache for task filters

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v37';
+const C = 'family-hub-v38';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './styles.css',
-        './app.js?v=36',
+        './app.js?v=37',
         './manifest.webmanifest'
       ])
     )
