c6ac6ba545e44bd31c35ff5eed1007803ee1a496
ModestlyMoist <brant@ziembas.net>
2026-09-15 16:59:42 -0700

    Refresh cache for More polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v49';
+const C = 'family-hub-v50';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './styles.css?v=47',
-        './app.js?v=48',
+        './app.js?v=49',
         './manifest.webmanifest'
       ])
     )
