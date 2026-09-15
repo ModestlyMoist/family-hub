333a1152fca859a9b94b781e2085a8a8884dfdf7
ModestlyMoist <brant@ziembas.net>
2026-09-15 16:52:47 -0700

    Refresh cache for shopping history

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v46';
+const C = 'family-hub-v47';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './styles.css?v=45',
-        './app.js?v=45',
+        './app.js?v=46',
         './manifest.webmanifest'
       ])
     )
