2ffbccd122896deb497bc3ee6300bcddb02bc51e
ModestlyMoist <brant@ziembas.net>
2026-09-16 13:47:54 -0700

    Refresh cache for Sports layout and filters

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v168a';
+const C = 'family-hub-v169a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -23,7 +23,7 @@ self.addEventListener('install', event => {
         './meals-shopping-polish.css?v=149a',
         './wellness2.css?v=154a',
         './meds.css?v=154a',
-        './sports.css?v=168a',
+        './sports.css?v=169a',
         './app.js?v=147a',
         './search.js?v=117a',
         './wellness.js?v=121a',
@@ -34,7 +34,7 @@ self.addEventListener('install', event => {
         './meals-shopping-polish.js?v=149a',
         './wellness2.js?v=154a',
         './meds.js?v=154a',
-        './sports.js?v=168a',
+        './sports.js?v=169a',
         './navfix.js?v=115a',
         './manifest.webmanifest'
       ])
