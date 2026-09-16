b464db6aa70617edb980838d5e33da5b9ac23727
ModestlyMoist <brant@ziembas.net>
2026-09-15 17:09:26 -0700

    Refresh cache for calendar Today shortcut

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v53';
+const C = 'family-hub-v54';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './styles.css?v=51',
-        './app.js?v=52',
+        './app.js?v=53',
         './manifest.webmanifest'
       ])
     )
