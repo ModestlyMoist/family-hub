c84db1a7608f72cffd4f613bd453e9f0408db94a
ModestlyMoist <brant@ziembas.net>
2026-09-15 21:34:59 -0700

    Refresh cache for smarter shopping

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v101a';
+const C = 'family-hub-v102a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -8,8 +8,8 @@ self.addEventListener('install', event => {
       cache.addAll([
         './',
         './index.html',
-        './styles.css?v=100a',
-        './app.js?v=101a',
+        './styles.css?v=102a',
+        './app.js?v=102a',
         './manifest.webmanifest'
       ])
     )
