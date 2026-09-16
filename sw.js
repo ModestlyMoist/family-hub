f8a51bf23d15ffd4ad5dc6bc9227fd8fcad011f3
ModestlyMoist <brant@ziembas.net>
2026-09-15 21:15:06 -0700

    Refresh cache for meal history navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v97a';
+const C = 'family-hub-v98a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './styles.css?v=96a',
-        './app.js?v=97a',
+        './app.js?v=98a',
         './manifest.webmanifest'
       ])
     )
