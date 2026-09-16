59f291e693bf1df60295652114ab560077ef4f12
ModestlyMoist <brant@ziembas.net>
2026-09-15 20:50:02 -0700

    Refresh cache for task progress ranges

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v90a';
+const C = 'family-hub-v91a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -8,8 +8,8 @@ self.addEventListener('install', event => {
       cache.addAll([
         './',
         './index.html',
-        './styles.css?v=90a',
-        './app.js?v=90a',
+        './styles.css?v=91a',
+        './app.js?v=91a',
         './manifest.webmanifest'
       ])
     )
