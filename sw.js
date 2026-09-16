1e0bbbaf454da0d1293ca0755f311638ab15eff1
ModestlyMoist <brant@ziembas.net>
2026-09-15 22:00:06 -0700

    Refresh cache for ingredient combining

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v104a';
+const C = 'family-hub-v105a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './styles.css?v=104a',
-        './app.js?v=104a',
+        './app.js?v=105a',
         './manifest.webmanifest'
       ])
     )
