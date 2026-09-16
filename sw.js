35880130fe847062cd1d8cbc7dcd92ee2670707e
ModestlyMoist <brant@ziembas.net>
2026-09-15 19:50:48 -0700

    Force cache to rolled back app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v80';
+const C = 'family-hub-v81';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './styles.css?v=78',
-        './app.js?v=79',
+        './app.js?v=80',
         './manifest.webmanifest'
       ])
     )
