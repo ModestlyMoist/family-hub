fc42509ce9416b9ec09d3c2b576c505be6e05208
ModestlyMoist <brant@ziembas.net>
2026-09-16 15:00:49 -0700

    Refresh Timothy TBall Giants team update v175a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v174a';
+const C = 'family-hub-v175a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -23,7 +23,7 @@ self.addEventListener('install', event => {
         './meals-shopping-polish.css?v=149a',
         './wellness2.css?v=154a',
         './meds.css?v=154a',
-        './sports.css?v=174a',
+        './sports.css?v=175a',
         './app.js?v=147a',
         './search.js?v=117a',
         './wellness.js?v=121a',
@@ -34,7 +34,7 @@ self.addEventListener('install', event => {
         './meals-shopping-polish.js?v=149a',
         './wellness2.js?v=154a',
         './meds.js?v=154a',
-        './sports.js?v=174a',
+        './sports.js?v=175a',
         './navfix.js?v=115a',
         './manifest.webmanifest'
       ])
