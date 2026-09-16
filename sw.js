1e41ae12ce186d71bd250b29a8d012f0cdcf5fc7
ModestlyMoist <brant@ziembas.net>
2026-09-15 21:26:44 -0700

    Refresh cache for Today page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v99a';
+const C = 'family-hub-v100a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -8,8 +8,8 @@ self.addEventListener('install', event => {
       cache.addAll([
         './',
         './index.html',
-        './styles.css?v=99a',
-        './app.js?v=98a',
+        './styles.css?v=100a',
+        './app.js?v=100a',
         './manifest.webmanifest'
       ])
     )
