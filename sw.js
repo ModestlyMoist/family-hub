3b7b2a8fadda0935c10785d48f764488d5f95ca8
ModestlyMoist <brant@ziembas.net>
2026-09-16 08:11:03 -0700

    Cache universal search module

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v116a';
+const C = 'family-hub-v117a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -10,7 +10,9 @@ self.addEventListener('install', event => {
         './index.html',
         './styles.css?v=114b',
         './navfix.css?v=115a',
+        './search.css?v=117a',
         './app.js?v=116a',
+        './search.js?v=117a',
         './navfix.js?v=115a',
         './manifest.webmanifest'
       ])
