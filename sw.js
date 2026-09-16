73f6f1b91597ff15fd96828bed9b7810eee158db
ModestlyMoist <brant@ziembas.net>
2026-09-16 13:58:46 -0700

    Refresh Sports flex child cards v173a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'family-hub-v172a';
+const C = 'family-hub-v173a';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -23,7 +23,7 @@ self.addEventListener('install', event => {
         './meals-shopping-polish.css?v=149a',
         './wellness2.css?v=154a',
         './meds.css?v=154a',
-        './sports.css?v=172a',
+        './sports.css?v=173a',
         './app.js?v=147a',
         './search.js?v=117a',
         './wellness.js?v=121a',
@@ -34,7 +34,7 @@ self.addEventListener('install', event => {
         './meals-shopping-polish.js?v=149a',
         './wellness2.js?v=154a',
         './meds.js?v=154a',
-        './sports.js?v=172a',
+        './sports.js?v=173a',
         './navfix.js?v=115a',
         './manifest.webmanifest'
       ])
